b21a1e09dd91e0798efa0de02e60e08755a09685
HGiang15 <gianghoang150503@gmail.com>
2024-05-16 22:45:50 +0700

    add feature

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,3 +6,24 @@ links.forEach((link) => {
     link.classList.add("navbar__link--active");
   });
 });
+
+// -- Function to scroll to the top of the page
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+}
+
+// Show the scroll-to-top button when scrolling down
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("scrollToTopBtn").style.display = "block";
+  } else {
+    document.getElementById("scrollToTopBtn").style.display = "none";
+  }
+}
