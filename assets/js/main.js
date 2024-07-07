ac66554eda0a7cab9231c067775dae64f96bb653
HGiang15 <gianghoang150503@gmail.com>
2024-07-07 22:10:12 +0700

    update Responsive 1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,10 @@
+// cop menu PC to menu Mobile
+const pcNav = document.querySelector("#pc-nav");
+const mobileNav = document.querySelector("#mobile-nav");
+
+// cop from PC nav to Mobile nav
+mobileNav.innerHTML = pcNav.innerHTML;
+
 const links = document.querySelectorAll(".navbar__link");
 const items = document.querySelectorAll(".navbar__item");
 
